c2f29d6a1731494e63c6ed3d311aa16028e5ef05
Queen of Corgis <tana.owens@gmail.com>
2020-07-22 16:14:39 -0600

    added extensions for employee types

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, gitHub) {
+    super(name, id, email);
+    this.gitHub = gitHub;
+  }
+    getRole() {
+      return "Engineer";
+    }
+    getgitHub() {
+      this.gitHub;
+    } 
+}
