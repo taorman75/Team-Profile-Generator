a65aadf71e848c5f4d1d86c483188eb50d74809a
Queen of Corgis <tana.owens@gmail.com>
2020-07-22 15:55:07 -0600

    created employee blueprint

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+const render = require("./htmlRenderer")
+
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+getName() {
+    return this.name;
+}
+getid() {
+    return this.id;
+}
+getEmail() {
+    return this.email;
+}
+getRole() {
+    return "Employee";
+}
+}
+
+module.exports = Employee
+
